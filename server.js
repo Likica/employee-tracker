4d87c1abad2d2468a81ca3e40762219691a6aafc
Coco Puff <fahrija_wyroski@live.com>
2021-05-17 12:48:31 -0500

    added db queries

--- server.js
+++ server.js
@@ -23,6 +23,35 @@ const db = mysql.createConnection(
 db.query(`SELECT * FROM employee`, (err, rows) => {
     console.log(rows);
 });
+//GET a single employee
+// db.query(`SELECT * FROM employee WHERE id=1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// DELETE an employee
+// db.query(`DELETE FROM employee WHERE id=?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+//CREATE an employee -- NOTES: In the SQL command we use the INSERT INTO command for the candidates table to add the values that are assigned to params. The four placeholders must match the four values in params, so we must use an array. Because the candidates table includes four columns—id, first_name, last_name, and industry_connected—we need four placeholders (?) for those four values. The values in the params array must match the order of those placeholders.
+
+// const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+// VALUES (?, ?, ?, ?)`;
+// const params = ['Lucky', 'Champ', 3];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
 
 // // get test route 
 // app.get('/', (req, res) => {
